105e878033316c773f061d8ed8569747208ca239
Collins Makamu <collinsmakamu95@gmail.com>
2023-08-07 22:16:01 +0300

    header component done

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import './header.css'
+import CTA from '../header/CTA'
+import HeaderSocials from './HeaderSocials'
+import CMM1 from '../../assets/cmm6.png'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Collins Makamu</h1>
+        <h5 className="text-light">FullStack Developer and Cybersecurity Enthusiast</h5> 
+        <CTA/> 
+        <HeaderSocials/>
+
+        <div className="me">
+          <img src={CMM1} alt="me" />
+        </div>
+
+        <a href="#contact" className="scroll__down">Scroll Down</a>
+      </div>
+    </header>
+
+  
+
   )
 }
 
