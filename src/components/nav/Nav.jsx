c3305f8b5dcf0eb4619e6b8fc6e0197938491cb3
Collins Makamu <collinsmakamu95@gmail.com>
2023-08-16 12:17:19 +0300

    Portfolio component done

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,24 +1,53 @@
-import React from 'react'
-import { useState } from 'react'
-import './nav.css'
-import {AiOutlineHome} from 'react-icons/ai'
-import {AiOutlineUser} from 'react-icons/ai'
-import {AiOutlineBook} from 'react-icons/ai'
-import {AiOutlineFolderOpen} from 'react-icons/ai'
-import {BiMessageSquareDetail} from 'react-icons/bi'
+import React from "react";
+import { useState } from "react";
+import "./nav.css";
+import { AiOutlineHome } from "react-icons/ai";
+import { AiOutlineUser } from "react-icons/ai";
+import { AiOutlineBook } from "react-icons/ai";
+import { AiOutlineFolderOpen } from "react-icons/ai";
+import { BiMessageSquareDetail } from "react-icons/bi";
 
 const Nav = () => {
-  const[activeNav, setActiveNav]=useState('#')
+  const [activeNav, setActiveNav] = useState("#");
   return (
     <nav>
-      <a href="#" onClick={()=> setActiveNav('#')} className={activeNav==='#' ? 'active':''}><AiOutlineHome/></a>
-      <a href="#about" onClick={()=> setActiveNav('#about')} className={activeNav==='#about' ? 'active':''}><AiOutlineUser/></a>
-      <a href="#experience"onClick={()=> setActiveNav('#experience')} className={activeNav==='#experience' ? 'active':''}><AiOutlineBook/></a>
-      <a href="#portfolio"onClick={()=> setActiveNav('#portfolio')} className={activeNav==='#portfolio' ? 'active':''}><AiOutlineFolderOpen/></a>
-      <a href="#contact"onClick={()=> setActiveNav('#contact')} className={activeNav==='#contact' ? 'active':''}><BiMessageSquareDetail/></a>
-      
+      <a
+        href="#"
+        onClick={() => setActiveNav("#")}
+        className={activeNav === "#" ? "active" : ""}
+      >
+        <AiOutlineHome />
+      </a>
+      <a
+        href="#about"
+        onClick={() => setActiveNav("#about")}
+        className={activeNav === "#about" ? "active" : ""}
+      >
+        <AiOutlineUser />
+      </a>
+      <a
+        href="#experience"
+        onClick={() => setActiveNav("#experience")}
+        className={activeNav === "#experience" ? "active" : ""}
+      >
+        <AiOutlineBook />
+      </a>
+      <a
+        href="#portfolio"
+        onClick={() => setActiveNav("#portfolio")}
+        className={activeNav === "#portfolio" ? "active" : ""}
+      >
+        <AiOutlineFolderOpen />
+      </a>
+      <a
+        href="#contact"
+        onClick={() => setActiveNav("#contact")}
+        className={activeNav === "#contact" ? "active" : ""}
+      >
+        <BiMessageSquareDetail />
+      </a>
     </nav>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,6 +1,20 @@
 import React from "react";
 import "./portfolio.css";
+import { Swiper, SwiperSlide } from "swiper/react";
 import PORT1 from "../../assets/bgflowers.jpg";
+import { BsGithub } from "react-icons/bs";
+import { CgMediaLive } from "react-icons/cg";
+import { BiSkipPrevious } from "react-icons/bi";
+import { BiSkipNext } from "react-icons/bi";
+
+// import Swiper core and required modules
+import { Pagination } from "swiper/modules";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/effect-coverflow";
+import "swiper/css/navigation";
 
 // DATA SET ? //
 
@@ -37,7 +51,7 @@ const data = [
     id: 5,
     image: PORT1,
     title: "PROJECT TITLE",
-    github: "https://github.com",
+    github: <a href=""></a>,
     demo: "https://dribble.com",
   },
   {
@@ -55,26 +69,49 @@ const Portfolio = () => {
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
 
-      <div className="container portfolio__container">
+      <Swiper
+        className="container portfolio__container"
+        modules={[Pagination]}
+        slidesPerView={3}
+        spaceBetween={30}
+        breakpoints={{
+          320: { slidesPerView: 1, spaceBetween: 10 },
+          375: { slidesPerView: 1, spaceBetween: 10 },
+          425: { slidesPerView: 1, spaceBetween: 10 },
+          640: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+          },
+        }}
+        pagination={{ clickable: true }}
+      >
         {data.map(({ id, image, title, github, demo }) => {
           return (
-            <article key={id} className="portfolio__item">
+            <SwiperSlide key={id} className="portfolio__item">
               <div className="portfolio__item-image">
                 <img src={image} alt={title} />
               </div>
               <h3>{title}</h3>
               <div className="portfolio__item-cta">
                 <a href={github} className="btn" target="_blank">
-                  Github
+                  <BsGithub />
                 </a>
                 <a href={demo} className="btn btn-primary" target="_blank">
-                  live
+                  <CgMediaLive className="portfolio__icon" />
                 </a>
               </div>
-            </article>
+            </SwiperSlide>
           );
         })}
-      </div>
+      </Swiper>
     </section>
   );
 };
