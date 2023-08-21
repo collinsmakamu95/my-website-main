56563650521a2ba43b399a2fde44c99dfb87a270
Collins Makamu <collinsmakamu95@gmail.com>
2023-08-21 16:04:31 +0300

    finals

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import './header.css'
-import CTA from '../header/CTA'
-import HeaderSocials from './HeaderSocials'
-import CMM1 from '../../assets/cmm6.png'
+import React from "react";
+import "./header.css";
+import CTA from "../header/CTA";
+import HeaderSocials from "./HeaderSocials";
+import CMM1 from "../../assets/cmm6.png";
 
 const Header = () => {
   return (
@@ -10,21 +10,22 @@ const Header = () => {
       <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1>Collins Makamu</h1>
-        <h5 className="text-light">FullStack Developer and Cybersecurity Enthusiast</h5> 
-        <CTA/> 
-        <HeaderSocials/>
+        <h5 className="text-light">
+          FullStack Developer and Cybersecurity Enthusiast
+        </h5>
+        <CTA />
+        <HeaderSocials />
 
         <div className="me">
           <img src={CMM1} alt="me" />
         </div>
 
-        <a href="#contact" className="scroll__down">Scroll Down</a>
+        <a href="#contact" className="scroll__down">
+          Scroll Down
+        </a>
       </div>
     </header>
+  );
+};
 
-  
-
-  )
-}
-
-export default Header
+export default Header;
